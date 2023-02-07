3630fdbdfe8b8d38687777e0906c471f3bdffc3e
Yang, Bo <pop.atry@gmail.com>
2023-02-06 17:32:31 -0800

    Add compression middleware

--- api/main.ts
+++ api/main.ts
@@ -9,9 +9,11 @@ import {
 import { fetchContributorStats } from '@/fetchContributorStats';
 import { isLocaleAvailable } from '@/translations';
 import express from 'express';
+import compression from 'compression';
 
 // Initialize Express
 const app = express();
+app.use(compression());
 
 // Create GET request
 app.get('/api', async (req, res) => {
